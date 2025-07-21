931dbe9c9d9ac911a4f3ec0e3abac4ca5612a6dc
ss-oh <tlstmddh12@gmail.com>
2025-07-21 18:00:55 +0900

    0721redux pages component등

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,16 @@
 import {useEffect, useState} from "react";
+import {Nav} from "react-bootstrap";
 import {useParams} from "react-router-dom";
+import TabContent from "../components/TabContent";
 
 function Detail({fruit}) {
   const {id} = useParams();
   const [num, setNum] = useState(0);
   const [num2, setNum2] = useState(0);
   const [alert, setAlert] = useState(true);
+  const [count, setCount] = useState(5);
+
+  const [tabNumber, setTabNumber] = useState(0);
 
   const selectedFruit = fruit[id];
 
@@ -29,8 +34,13 @@ function Detail({fruit}) {
   // 의존성 배열이 빈 배열이면 마운트 시 한번만 실행이 됨
   // 의존성 배열에 특정 state,props가 있으면 마운트 될때와 해당 state,props가 업데이트 될 때 실행이 된다.
   useEffect(() => {
-    console.log("useEffet확인용 콘솔");
-  }, [num]);
+    if (count > 0) {
+      const timer = setTimeout(() => {
+        setCount(count - 1);
+      }, 1000);
+      return () => clearTimeout(timer); // 정리
+    }
+  }, [count]);
 
   if (!selectedFruit) {
     return <div>해당 상품이 없습니다.</div>;
@@ -54,8 +64,11 @@ function Detail({fruit}) {
         버튼
       </button>
       {num2}
+
       {alert ? (
-        <div className="alert alert-danger">5초안에 구매하면 공짜</div>
+        <div className="alert alert-danger">
+          {`${count}초 안에 구매하면 공짜`}
+        </div>
       ) : (
         ""
       )}
@@ -74,6 +87,40 @@ function Detail({fruit}) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav className="mt-5" variant="tabs" justify defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-0"
+            onClick={() => {
+              setTabNumber(0);
+            }}
+          >
+            상세정보
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-1"
+            onClick={() => {
+              setTabNumber(1);
+            }}
+          >
+            리뷰
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            eventKey="link-2"
+            onClick={() => {
+              setTabNumber(2);
+            }}
+          >
+            반품,교환정보
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tabNumber={tabNumber} />
     </div>
   );
 }
